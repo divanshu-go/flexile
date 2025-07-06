18ddc8f243e21d88ffdfd6c84340c6f5fd545b7a
Maya <19721695+code-elf@users.noreply.github.com>
2025-07-07 01:21:05 +0200

    fix

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -206,7 +206,7 @@ class User < ApplicationRecord
   end
 
   def should_regenerate_consulting_contract?(changeset)
-    CONSULTING_CONTRACT_ATTRIBUTES.any? do |attr|
+    !contract_signed_elsewhere? && CONSULTING_CONTRACT_ATTRIBUTES.any? do |attr|
       changeset[attr].present? && send(attr) != changeset[attr]
     end
   end
